3bd89ce79353602d3a818c51a75246691f7e13da
Pashezik <pashezik@yandex.ru>
2019-03-31 10:47:26 +0000

    Add ansible

---
+++ ansible/roles/docker/molecule/default/tests/test_default.py
@@ -0,0 +1,10 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+def test_docker_works(host):
+    assert host.run('docker run hello-world').rc == 0
